0d0af5e46d0f12b9d40566519eaa4bda66e51c3c
muhammad Khair <ahmadkhair853@gmail.com>
2025-10-01 15:31:10 +0800

    feat: Menambahkan fungsionalitas CRUD untuk Manajemen Jabatan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\KontenController;
+use App\Http\Controllers\JabatanController;
 
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 });
 
 Route::resource('berita', KontenController::class);
+Route::resource('jabatan', JabatanController::class);
